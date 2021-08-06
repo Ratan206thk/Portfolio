e3f8d86f6fa7703b6c1837adc894fe04c9a6c11a
Ratnakar Thakur <39662239+Ratan206thk@users.noreply.github.com>
2021-08-07 04:01:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function opening() {
+    document.querySelector('div.hello').style.opacity = 0.8;
+    document.querySelector('div.hello').style.visibility = "visible";
+}
+function closing() {
+    document.querySelector('div.hello').style.opacity = 0;
+    document.querySelector('div.hello').style.visibility = "hidden";
+}
