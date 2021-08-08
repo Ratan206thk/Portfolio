01c7b7df61b6391d00e135cd772fae2b9ee9746d
Ratan206thk <Ratan206thk@gmail.com>
2021-08-09 01:25:24 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function opening() {
     document.querySelector('div.hello').style.opacity = 0.8;
+    document.querySelector('div.box').style.position = 'fixed';
     document.querySelector('div.hello').style.visibility = "visible";
 }
 function closing() {
     document.querySelector('div.hello').style.opacity = 0;
+    document.querySelector('div.box').style.position = 'absolute';
     document.querySelector('div.hello').style.visibility = "hidden";
 }
