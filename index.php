3956452b0b5e7f803448c82b843dbb0a1fc070ad
Ratan206thk <Ratan206thk@gmail.com>
2021-08-08 23:55:47 +0530

    update

--- index.php
+++ index.php
@@ -6,45 +6,44 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="./style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./style.css">
   <title>Header</title>
 </head>
 
 <body>
   <div class="box">
-    <nav class="navbar navbar-light bg-light">
-      <div class="container justiy-content-between">
+    <nav class="navbar">
+      <div class="container">
         <a class="navbar-brand">
-          <img src="./img1.png" alt="" width="100">
+          <img src="./images/Group 2536.png" alt="" width="90">
         </a>
         <a class="navbar-brand" onclick="opening()">
-          <img src="./img2.png" alt="" width="80">
+          <img src="./images/bx-menu-alt-right.svg" alt="" width="30">
         </a>
       </div>
     </nav>
-    <div id='container'>
+    <div class='d-flex justify-content-around flex-md-row'>
       <div class='child'>
         <h1>Chintan Thakur</h1>
         <span>UX/UI Designer & Consultant</span>
         <hr size="7" width="70">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus commodi numquam quae atque. Eligendi
-          cumque, optio ipsa totam amet excepturi, veritatis quod ut laboriosam autem eaque neque, similique reiciendis
-          voluptate?</p>
+        <p>Hello. My name is Chintan, I'm skilled Web Designer with a wide range of expertise in UI and UX Design. I have good experience in creating web, mobile and tablet user interfaces and experiences from both, creative and analytical sides. I am able to plan, budget and manage an entire site or app creation process from start to finish.</p>
         <button>Work together</button>
       </div>
-      <div class="child">
-        <img src="./img3.png">
-      </div>
+      <img class="img-fluid" src="./images/Avatar.png" width="42%" height="auto">
     </div>
   </div>
-  <div class="hello box">
+  <!-- <div class="hello box">
     <img src="./img4.png" onclick="closing();">
     <ul>About me</ul>
     <ul>Creation Process</ul>
     <ul>Portolio</ul>
     <ul>Testimonials</ul>
     <ul>Contact me</ul>
-  </div>
+  </div> -->
 </body>
 <script src="./script.js"></script>
 </html>
