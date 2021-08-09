10320d00453ffdaaebe58aeb8a6936ebbb185014
Ratan206thk <Ratan206thk@gmail.com>
2021-08-09 15:27:34 +0530

    update

--- index.php
+++ index.php
@@ -121,7 +121,9 @@
           <div class="card h-100">
             <div class="card-body">
               <h3><b><span>4 </span> IA & Wireframes</b></h3>
-              <p>Review project information (users & business). This is mandatory to avoid possible and unnecessary changes in the deliverables. We create initial wireframes and mid-fidelity prototypes. Put prototypes to test and get feedback from users.</p>
+              <p>Review project information (users & business). This is mandatory to avoid possible and unnecessary
+                changes in the deliverables. We create initial wireframes and mid-fidelity prototypes. Put prototypes to
+                test and get feedback from users.</p>
               <h3><b>Resulting Assets</b></h3>
               <div class='sub'>
                 <button class='get'>Mid-Fi Prototype</button>
@@ -135,7 +137,9 @@
           <div class="card h-100">
             <div class="card-body">
               <h3><b><span>5 </span> Hi-Fi Design</b></h3>
-              <p>The product gets is final face . Here we apply the design system to that wireframe which gets the final design of the product . Creating Hi -fidelity prototype , testing , getting feedback and redesign if needed</p>
+              <p>The product gets is final face . Here we apply the design system to that wireframe which gets the final
+                design of the product . Creating Hi -fidelity prototype , testing , getting feedback and redesign if
+                needed</p>
               <h3><b>Resulting Assets</b></h3>
               <div class='sub'>
                 <button class='get'>Hi-Fi Prototype</button>
@@ -150,7 +154,8 @@
           <div class="card h-100">
             <div class="card-body">
               <h3><b><span>6 </span> Client Deliverables</b></h3>
-              <p>Finally after all the steps and loops ended the design assets are being generated and validated. It'll be delivered in the way that you and your development team need.</p>
+              <p>Finally after all the steps and loops ended the design assets are being generated and validated. It'll
+                be delivered in the way that you and your development team need.</p>
               <h3><b>Resulting Assets</b></h3>
               <div class='sub'>
                 <button class='get'>Final Prototype</button>
@@ -158,6 +163,134 @@
                 <button class='get'>Dev.assets</button>
                 <button class='get'>Functional Documentation</button>
                 <button class='get'>Design Specifications</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class='creation'>
+      <h6>What Client's say's?</h6>
+      <h1>Testimonials</h1>
+      <div class="row row-cols-1 row-cols-sm-3 g-4 flex-row flex-nowrap">
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"Chintan is one of the best freelancers I have worked with so far. He is extremely dedicated and
+                passionate about his work. The quality that I have received from him is extremely satisfying. Actually,
+                he understood my vision and brought it into life exactly the way I was expecting. Starting from the
+                wireframe, designing the logo, placing every page, creating the prototype he helped me with every aspect
+                of it and also provided me important suggestions to incorporate into my initial plan. I have received my
+                final prototype in just 10days with all the change requests during the design. <br><br>
+                He is extremely talented and a good human being, I would highly recommend him and if I get a chance I
+                would definitely work with him in the future."</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Souvik S</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"He is amazing and very hard working. He understands your vision and works very hard to achieve it. You
+                can trust him and the quality of work is really great. He provide full support and give regular updates
+                of progress"</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Vedantt S</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"Chintan has really good knowledge on designing modern UI with simplify user experience. I have worked
+                with many designer for my product design and I could say he was the best one to understand my
+                requirement and fulfil it as per my choice. I enjoyed working with him as he never forced me to accept
+                his design if I didn't like it. He keep insisted me to set my expectations high on design part so he can
+                do his job more better.<br><br>Definitely he is very cooperative and would be the first guy for me to
+                reach out for any UI/UX related work.<br><br>Keep up the good work Chintan!!"</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Chirag A</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"I worked with Chintan for 3 months and very happy with his work. He is a thorough professional -
+                extremely well behaved with a positive attitude. He has a solution for every problem. Chintan is
+                available whenever needed and has shown great sense of urgency in meeting the project
+                schedules.<br><br>Keep it up Chintan!!"</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Gautam B</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"Chintan is a great designer to work with. He brings in great designs, good experience and is extremely
+                co-operative. Will recommend him to everyone and will rework with him when required. <br><br> Great work
+                Chintan, and all the best."</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Farhan J</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/upwork.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"is one of the most enthusiastic and exited designers ! have delt with. Since day 1, he showed his
+                skills from Planning, constructing and developing the wireframes. <br><br>
+                He understands the business well and also pictures the problem and breaks them in to small pieces to
+                minify the complex problems, especially for one of the challenges we are facing, he jumped in and solved
+                it very easily. He understands the USA rules and complaisance always learns new topics to make his
+                productivity as clean and deliberate as possible."</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Sukhesh G</b></h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="navbar-brand">
+              <img src="./images/linkedin1.svg" class="d-inline-block align-text-top">
+            </div>
+            <div class="card-body">
+              <p>"Chintan is the kind of Designer you can only dream to get! His humility combined with his ability to understand abstract requirements or sync with your thoughts is his secret weapon. A brilliant designer who has kept up with the global best practices and speedy execution. At Revidya, we went through at least 10 diff product iterations and 3 diff company formats - all of which Chintan was able to glide through and ace beautifully. Every good freelancer also needs great interpersonal and team work skills another area where Chintan showed exemplary teamwork abilities. We've received feedback from clients that our digital assets look really professional. I would blindly recommend Chintan to anyone who's seeking to build small, medium or large scalable MVPs."</p>
+              <div class="rating">
+                <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                <h5><b>Sukhesh G</b></h5>
               </div>
             </div>
           </div>
