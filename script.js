a50f77e315b4cba2ab1e383d0368009745564bd1
Ratan206thk <Ratan206thk@gmail.com>
2021-08-10 22:01:22 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function closing() {
     document.querySelector('div.box').style.position = 'absolute';
     document.querySelector('div.hello').style.visibility = "hidden";
 }
+// let arr=document.querySelectorAll('i')
+// arr[0].addEventListener('click',()=>{
+//     let elmnt = document.querySelector("div.scroll");
+//     elmnt.scrollLeft = -420;
+// })
+// arr[1].addEventListener('click',()=>{
+//     let elmnt = document.querySelector("div.scroll");
+//     elmnt.scrollLeft = 420;
+// })
+
+
+// document.queryselector('button').addEventListener('click',()=>{
+//     var elmnt = document.getElementById("myDIV");
+//     elmnt.scrollLeft = 50;
+//   });
